fea89e8369e811bc0c8a1e956255db761c48cf21
Nathan Hollis <natehollis@gmail.com>
2020-02-19 12:04:15 -0600

    Add DateTime extensions

---
+++ Csharp.Utilities/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace DataPowerTools.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// To a file date time string  e.g. "2_19_2020 12_03_36 PM"
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static string ToFileDateTimeString(this DateTime time)
+        {
+            var fileDate = time.ToString(CultureInfo.InvariantCulture).Replace("/", "_").Replace(":", "_");
+
+            return fileDate;
+        }
+    }
+}
